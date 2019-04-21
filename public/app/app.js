a1f8f18c50eed761b5a5ad38b433d2ee0fe27be8
RenatoFrancisco <renatofrancisco.s@gmail.com>
2019-04-21 15:06:01 -0300

    06 Implementando a função flatMap.

--- public/app/app.js
+++ public/app/app.js
@@ -1,9 +1,14 @@
 import { handleStatus, log } from './utils/promise-helper.js';
+import './utils/array-helper.js';
 
 document
     .querySelector('#myButton')
     .onclick = () =>
         fetch('http://localhost:3000/notas')
         .then(handleStatus)
+        .then(notas => notas
+            .$flatMap(nota => nota.itens)
+            .filter(item => item.codigo == '2143')
+            .reduce((total, item) => total + item.valor, 0))
         .then(log)
         .catch(log);
