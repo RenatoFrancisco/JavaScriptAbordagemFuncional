6636073c1ad9b3c7d57b7b01354d30de161c1251
RenatoFrancisco <renatofrancisco.s@gmail.com>
2019-04-21 16:51:29 -0300

    10 A classe de serviço.

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,12 @@
 import { handleStatus, log } from './utils/promise-helper.js';
 import './utils/array-helper.js';
+import { notasService as service } from './nota/service.js';
 
-const sumItems = code => notas => notas
-    .$flatMap(nota => nota.itens)
-    .filter(item => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0)
+
 
 document
     .querySelector('#myButton')
     .onclick = () =>
-        fetch('http://localhost:3000/notas')
-        .then(handleStatus)
-        .then(sumItems('2143'))
+        service.sumItems('2143')
         .then(log)
         .catch(log);
