04ae1eb87114bd417f4c4ba15216d0bdd56bcfce
RenatoFrancisco <renatofrancisco.s@gmail.com>
2019-04-21 14:29:09 -0300

    10  Consumindo endpoint REST com a API Fetch.

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,9 @@
+import { handleStatus } from './utils/promise-helper.js';
+
 document
     .querySelector('#myButton')
-    .onclick = () => alert('oi');
+    .onclick = () =>
+        fetch('http://localhost:3000/notas')
+        .then(handleStatus)
+        .then(notas => console.log(notas))
+        .catch(console.log);
