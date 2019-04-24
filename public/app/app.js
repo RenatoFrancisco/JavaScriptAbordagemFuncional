ab906995694c2353704f8252436c80393feaba77
RenatoFrancisco <renatofrancisco.s@gmail.com>
2019-04-23 23:05:09 -0300

    03 Limitando operações - debounceTime.

--- public/app/app.js
+++ public/app/app.js
@@ -1,14 +1,16 @@
 import { log } from './utils/promise-helper.js';
 import './utils/array-helper.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 
-const operation1 = takeUntil(3, () =>
-    service.sumItems('2143')
-    .then(log)
-    .catch(log)
+const action = debounceTime(500, 
+    takeUntil(3, () =>
+        service.sumItems('2143')
+        .then(log)
+        .catch(log)
+    )
 );
 
 document
     .querySelector('#myButton')
-    .onclick = operation1;
+    .onclick = action;
