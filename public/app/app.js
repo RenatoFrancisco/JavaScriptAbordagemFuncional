cc230ca55f664f0ba672acf7c501197f0e8d14d3
RenatoFrancisco <renatofrancisco.s@gmail.com>
2019-04-23 22:50:24 -0300

    01 Limitando operações - takeUntil.

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,14 @@
-import { handleStatus, log } from './utils/promise-helper.js';
+import { log } from './utils/promise-helper.js';
 import './utils/array-helper.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
 
-
+const operation1 = takeUntil(3, () =>
+    service.sumItems('2143')
+    .then(log)
+    .catch(log)
+);
 
 document
     .querySelector('#myButton')
-    .onclick = () =>
-        service.sumItems('2143')
-        .then(log)
-        .catch(log);
+    .onclick = operation1;
